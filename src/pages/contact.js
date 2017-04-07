a2629b05bc53581c89560d6b72bc24aa41d1ef35
alon bar david <illniyar@gmail.com>
2017-04-07 13:57:33 +0300

    changed email to new me@alonbd.com

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function ContactPage(){
     return <div>
         <h2>Contact</h2>
-        <div>You can reach me by email at <a href="mailto:me@alonbardavid.com">me@alonbardavid.com</a></div>
+        <div>You can reach me by email at <a href="mailto:me@alonbd.com">me@alonbd.com</a></div>
     </div>
 }
