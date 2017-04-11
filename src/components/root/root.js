a22c49f0751832f4541797226f303ee5122b1ec0
alon bar david <illniyar@gmail.com>
2017-04-11 14:07:30 +0300

    added propert OG meta-tag support

--- src/components/root/root.js
+++ src/components/root/root.js
@@ -3,6 +3,7 @@ import Nav from 'src/components/nav/nav';
 import Helmet from 'react-helmet';
 import Footer from 'src/components/footer/footer';
 import PageTransition from 'src/components/page-transition';
+import LogoUrl from 'src/img/logo.png';
 import './root.scss';
 
 export default class RootComponent extends React.Component {
@@ -10,12 +11,15 @@ export default class RootComponent extends React.Component {
     render() {
         const props = this.props;
         const normalizedPath = props.path.replace("/","-");
+        const title = `Alon Bar David | ${props.meta.title}`;
         const meta = {
             ...props.meta,
-            title: `Alon Bar David | ${props.meta.title}`,
-            meta: (props.meta.meta || []).concat({
-                name:"description", content:props.meta.description
-            })
+            title: title,
+            meta: (props.meta.meta || []).concat([
+                {name: "og:description", content: props.meta.description},
+                {name:"og:title", content:title},
+                {name:"og:image", content:LogoUrl}
+            ])
         }
         return (<div id={`page-${normalizedPath}`}>
             <Nav path={props.path}></Nav>
