d047d0d1b7d231b6d5064a29083bd46ff0950143
Alon Bar David <me@alonbd.com>
2022-08-11 08:37:12 +0300

    naming things first iteration

---
+++ naming-things/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Naming things</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NamingThingsPage",
+  computed:{
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
